81e96e5a82bc33b2c699af830354953f782f0a5e
zxhhhxz1221 <1305894626@qq.com>
2024-08-05 16:50:18 +0800

    验证邮件发送功能...
    本地版本才能运行，未上传本地版本。

---
+++ src/main/java/da20240805/MyEmailUtilTest_upload.java
@@ -0,0 +1,65 @@
+package da20240805;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+
+import java.util.Properties;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/8/5 16:45
+ */
+public class MyEmailUtilTest_upload {
+    // 1. state (filed、variable)
+    private static final String FROM = "你自己的QQ邮箱@qq.com"; // 邮件发送者的身份
+    private static final String SECRET_TOKEN = "开启邮箱发送服务之后获取到的授权码（注意：不是登录秘密哦）";
+
+    // 2. behavior (method、function)
+    public static void send(String email, String content)  throws Exception {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", "smtp.qq.com");
+        prop.put("mail.smtp.port", "587");
+
+        // https://wx.mail.qq.com/list/readtemplate?name=app_intro.html#/agreement/authorizationCode
+        Session session = Session.getInstance(prop, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM, SECRET_TOKEN);
+            }
+        });
+
+        Message message = new MimeMessage(session);
+        // who you are
+        message.setFrom(new InternetAddress(FROM));
+        // send to ...
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+
+        message.setSubject("来自 Java 的提醒");
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(content, "text/html; charset=utf-8");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+
+        Transport.send(message);
+
+        System.out.println("邮件发送成功");
+    }
+
+    // 3. constructor
+
+    // other
+    public static void main(String[] args) throws Exception {
+        MyEmailUtilTest_upload.send("你要发送给谁@qq.com", "这是一段测试消息");
+    }
+}
