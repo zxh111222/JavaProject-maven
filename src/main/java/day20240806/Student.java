f2b259c866f57e0b52783b545ca10076bef5fa52
zxhhhxz1221 <1305894626@qq.com>
2024-08-06 11:42:39 +0800

    Update Student (error)

--- src/main/java/day20240806/Student.java
+++ src/main/java/day20240806/Student.java
@@ -17,36 +17,7 @@ public class Student {
     int age;
 
     //constructor
-    public Student() {
-    }
-
-    public Student(String name) {
-        this.newName = name;
-    }
-
-    public Student(int age) {
-        this.age = age;
-    }
-
-    public Student(String name, int age) {
-        this.newName = name;
-        this.age = age;
-    }
 
     //behavior
-    public String getNewName() {
-        return newName;
-    }
 
-    public void setNewName(String name) {
-        this.newName = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
