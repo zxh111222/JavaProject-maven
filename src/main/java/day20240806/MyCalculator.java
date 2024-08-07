05110b5740d69bd11cf710b1414151b01d17916d
zxhhhxz1221 <1305894626@qq.com>
2024-08-07 17:16:19 +0800

    增加 MyCalculator.java

---
+++ src/main/java/day20240806/MyCalculator.java
@@ -0,0 +1,13 @@
+package day20240806;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/8/6 15:56
+ */
+public class MyCalculator {
+    public static int add(int a, int b){
+        return a + b + 1;
+    }
+}
