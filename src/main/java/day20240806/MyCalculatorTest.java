efeb33d9f6b3ee8e8bd4031d9ac12705e720023a
zxhhhxz1221 <1305894626@qq.com>
2024-08-07 17:16:29 +0800

    增加 MyCalculatorTest.java

---
+++ src/main/java/day20240806/MyCalculatorTest.java
@@ -0,0 +1,16 @@
+package day20240806;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/8/6 15:57
+ */
+public class MyCalculatorTest {
+    public static void main(String[] args) {
+        int sum = MyCalculator.add(2, 3);
+        System.out.println(sum);
+        System.out.println(sum == 5);
+    }
+
+}
