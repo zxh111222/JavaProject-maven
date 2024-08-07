030902383e5c3b76c83a3df09ff59ffe54cbd1e3
zxhhhxz1221 <1305894626@qq.com>
2024-08-07 17:15:34 +0800

    由 lombok 引出 record 关键字，关键是背后的思想

---
+++ src/main/java/day20240806/Teacher.java
@@ -0,0 +1,10 @@
+package day20240806;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/8/7 17:09
+ */
+public record Teacher(String name, int age) {
+}
---
+++ src/main/java/day20240806/TeacherTest.java
@@ -0,0 +1,15 @@
+package day20240806;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/8/7 17:10
+ */
+public class TeacherTest {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher("t1", 25);
+        System.out.println(t1.name());
+        System.out.println(t1.age());
+    }
+}
