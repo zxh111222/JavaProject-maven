9f065ea2c3d4b9a98fb8cd0d64be5a9b5a2fe970
zxhhhxz1221 <1305894626@qq.com>
2024-08-12 16:30:19 +0800

    引出 JUnit5 的 @BeforeAll 和 @AfterAll

--- src/test/java/day20240812/JDBCTest.java
+++ src/test/java/day20240812/JDBCTest.java
@@ -1,5 +1,7 @@
 package day20240812;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
@@ -11,6 +13,22 @@ import java.sql.*;
  * @date 2024/8/12 16:23
  */
 public class JDBCTest {
+
+    static Connection connection;
+    @BeforeAll
+    public static void getConnection() throws SQLException {
+        // 获取 Connection (连接到 MySQL 服务器)
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo01", "root", "secret_mysql");
+        System.out.println("Connected to database");
+    }
+
+    @AfterAll
+    public static void closeConnection() throws SQLException {
+        // 关闭连接
+        connection.close();
+        System.out.println("close connection");
+    }
+
     @Test
     public void testStatement() throws SQLException {
         // 获取 Connection (连接到 MySQL 服务器)
@@ -27,15 +45,10 @@ public class JDBCTest {
             int balance = rs.getInt("balance");
             System.out.println(id + "\t" + name + "\t" + balance);
         }
-        // 关闭连接
-        connection.close();
     }
 
     @Test
     public void testPreparedStatement() throws SQLException {
-        // 获取 Connection (连接到 MySQL 服务器)
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo01", "root", "123456");
-        System.out.println("Connected to database");
         // 对 `MySQL 服务器` 讲它听得懂的话：SQL 之 PreparedStatement
         String query = "select id, name, balance from user where id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
@@ -48,8 +61,6 @@ public class JDBCTest {
             int balance = rs.getInt("balance");
             System.out.println(id + "\t" + name + "\t" + balance);
         }
-        // 关闭连接
-        connection.close();
     }
 
     @Test
