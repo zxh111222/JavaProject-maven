70bc25bf7dbc95ac07a8938a384762745eb67058
zxhhhxz1221 <1305894626@qq.com>
2024-08-12 11:05:02 +0800

    homework - DbStorage

---
+++ src/main/java/day20240811/phase_project/notificator/ConsoleNotificator.java
@@ -0,0 +1,35 @@
+package day20240811.phase_project.notificator;
+
+
+import day20240811.phase_project.dto.CustomResult;
+
+import java.util.List;
+import java.util.Properties;
+
+import static day20240811.phase_project.App.PROPERTIES;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/6/11 9:01
+ */
+public class ConsoleNotificator implements Notificator {
+    @Override
+    public void notify(List<CustomResult> information) {
+        // 获取信息并发送到控制台
+        String msg = Notificator.getMsgFromResult(information);
+        if (!msg.isBlank()) {
+            String to = PROPERTIES.getProperty("to");
+            System.out.println("--- --- ---");
+            System.out.println("Hi, " + to);
+            System.out.println();
+            System.out.println(msg);
+            System.out.println("--- --- ---");
+            System.out.println("发送完毕!");
+        } else {
+            System.out.println("没有命中任何关键词，无需发送通知");
+        }
+
+    }
+}
---
+++ src/main/java/day20240811/phase_project/storage/ConsoleStorage.java
@@ -0,0 +1,27 @@
+package day20240811.phase_project.storage;
+
+
+import day20240811.phase_project.dto.CustomResult;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author XinhaoZheng
+ * @version 1.0
+ * @description: TODO
+ * @date 2024/6/9 14:14
+ */
+public class ConsoleStorage implements Storage {
+    @Override
+    public void save(List<CustomResult> information) {
+        for (CustomResult cr : information) {
+            System.out.println(cr.getTitle());
+            System.out.println(cr.getUrl());
+            System.out.println(cr.getCreatedAt());
+            System.out.println(cr.getUpdatedAt());
+            System.out.println("--- --- ---");
+        }
+    }
+}
