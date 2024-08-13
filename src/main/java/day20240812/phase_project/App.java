153bfbc6681f20560c11329e412a4771366be508
zxhhhxz1221 <1305894626@qq.com>
2024-08-13 18:45:13 +0800

    homework - app - 数据库中增加任务表，并实现功能...
    可以同时解析不同网站，目前可以运行，但只有厦门小鱼网

--- src/main/java/day20240812/phase_project/App.java
+++ src/main/java/day20240812/phase_project/App.java
@@ -8,6 +8,7 @@ import day20240812.phase_project.storage.Storage;
 
 import java.io.*;
 import java.sql.*;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -23,6 +24,8 @@ import java.util.Properties;
 public class App {
 
     public static final Properties PROPERTIES;
+    private static Connection CONNECTION;
+    private static Map<String, String> URLPARSERMAP = new HashMap<>();
 
     static {
         PROPERTIES = loadFromConfiguration();
@@ -44,31 +47,43 @@ public class App {
         System.out.println("程序名称 : " + app);
         System.out.println("程序开始运行...");
 
-        // 下载模块: 根据配置文件的信息进行不同方式的下载
-        String url = PROPERTIES.getProperty("url");
-        System.out.println("Downloader - 正在下载...:" + url);
-        Downloader downloader = Downloader.getInstance(PROPERTIES.getProperty("downloader"));
-        String html = downloader.download(url);
-        System.out.println("Downloader -下载完成");
+        for (Map.Entry<String, String> site : URLPARSERMAP.entrySet()) {
+            String urlName = PROPERTIES.getProperty("urlName");
+            String url = site.getKey();
+            String url_parser = site.getValue();
 
-        // 解析模块: 根据配置文件的信息进行不同 url 的解析
-        System.out.println("Parser - 正在解析...");
-        Parser parser = Parser.getInstance(PROPERTIES.getProperty("parser"));
-        List<CustomResult> information = parser.parse(html);
-        System.out.println("Parser - 解析完成！(共获取到" + information.size() + "条数据)");
-        // System.out.println(information);
+            if (url_parser == null) {
+                System.out.println("没有找到解析器配置，跳过 URL: " + url);
+                continue;
+            }
 
-        // 存储模块: 根据配置文件的信息进行不同方式的存储
-        System.out.println("Storage - 正在存储...");
-        Storage storage = Storage.getInstance(PROPERTIES.getProperty("storage"));
-        storage.save(information);
-        System.out.println("Storage - 存储完成...");
+            System.out.println("当前下载、解析的网站是:" + urlName);
 
-        // 通知模块: 根据配置文件的信息选择通知方式，配置文件含有通知的相关信息
-        System.out.println("Notificator - 正在通知...");
-        Notificator notificator = Notificator.getInstance();
-        notificator.notify(information);
-        System.out.println("Notificator - 通知完成...");
+            // 下载模块: 根据配置文件的信息进行不同方式的下载
+            System.out.println("Downloader - 正在下载...:" + url);
+            Downloader downloader = Downloader.getInstance(PROPERTIES.getProperty("downloader"));
+            String html = downloader.download(url);
+            System.out.println("Downloader -下载完成");
+
+            // 解析模块: 根据配置文件的信息进行不同 url 的解析
+            System.out.println("Parser - 正在解析...");
+            Parser parser = Parser.getInstance(url_parser);
+            List<CustomResult> information = parser.parse(html);
+            System.out.println("Parser - 解析完成！(共获取到" + information.size() + "条数据)");
+            // System.out.println(information);
+
+            // 存储模块: 根据配置文件的信息进行不同方式的存储
+            System.out.println("Storage - 正在存储...");
+            Storage storage = Storage.getInstance(PROPERTIES.getProperty("storage"));
+            storage.save(information);
+            System.out.println("Storage - 存储完成...");
+
+            // 通知模块: 根据配置文件的信息选择通知方式，配置文件含有通知的相关信息
+            System.out.println("Notificator - 正在通知...");
+            Notificator notificator = Notificator.getInstance();
+            notificator.notify(information);
+            System.out.println("Notificator - 通知完成...");
+        }
 
         //结束
         System.out.println("程序运行结束!");
@@ -76,22 +91,35 @@ public class App {
 
     private static Properties loadFromConfiguration() {
         Properties properties = new Properties();
-        String query = "SELECT `config_name`, `key`, `value` FROM app_config WHERE app_name = ? AND version = ?";
+        String appConfigQuery  = "SELECT `config_name`, `key`, `value` FROM app_config WHERE app_name = ? AND version = ?";
+        String urlParseQuery = "SELECT * FROM url_parse"; // URL 解析配置的查询
+
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "root", "123456");
-            PreparedStatement pstmt = connection.prepareStatement(query);
+            CONNECTION = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "root", "123456");
+            PreparedStatement appConfigPstmt  = CONNECTION.prepareStatement(appConfigQuery );
 
-            pstmt.setString(1, "阶段项目");
-            pstmt.setString(2, "1.0");
+            appConfigPstmt.setString(1, "阶段项目");
+            appConfigPstmt.setString(2, "1.0");
 
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                while (resultSet.next()) {
-                    String config_name = resultSet.getString("config_name");
-                    String value = resultSet.getString("value");
-                    properties.setProperty(config_name, value);
-                }
-                System.out.println("配置文件从数据库读取成功！");
+            ResultSet appConfigResultSet  = appConfigPstmt .executeQuery();
+            while (appConfigResultSet.next()) {
+                String config_name = appConfigResultSet .getString("config_name");
+                String value = appConfigResultSet .getString("value");
+                properties.setProperty(config_name, value);
             }
+            // 读取 URL 解析配置
+            PreparedStatement urlParsePstmt = CONNECTION.prepareStatement(urlParseQuery);
+            ResultSet urlParseResultSet = urlParsePstmt.executeQuery();
+            while (urlParseResultSet.next()) {
+                String urlName = urlParseResultSet.getString("name");
+                String url = urlParseResultSet.getString("url");
+                String parser = urlParseResultSet.getString("parser");
+                URLPARSERMAP.put(url, parser);
+                properties.setProperty("urlName", urlName);
+                properties.setProperty("url", url);
+                properties.setProperty("parser", parser);
+            }
+            System.out.println("配置文件从数据库读取成功！");
         } catch (SQLException e) {
             System.out.println("从数据库读取配置时出错: " + e.getMessage());
             throw new RuntimeException(e);
--- src/main/java/day20240812/phase_project/resources/app.sql
+++ src/main/java/day20240812/phase_project/resources/app.sql
@@ -14,7 +14,7 @@ CREATE TABLE `app_config` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `app_name` VARCHAR(50) NOT NULL,
     `version` VARCHAR(50) NOT NULL,
-    `config_name` VARCHAR(255) NOT NULL,     -- 配置集的名称
+    `config_name` VARCHAR(255) NOT NULL,
     `key` VARCHAR(255) NOT NULL,
     `value` VARCHAR(255) NOT NULL
 );
@@ -24,8 +24,7 @@ VALUES
     ('阶段项目', '1.0', 'app', 'app', '阶段项目'),
     ('阶段项目', '1.0', 'version', 'version', '1.0'),
     ('阶段项目', '1.0', 'downloader', 'MyIODownloader', 'day20240812.phase_project.downloader.MyIODownloader'), -- JsoupDownloader
-    ('阶段项目', '1.0', 'url', 'xmfishURL', 'http://bbs.xmfish.com/thread-htm-fid-55-search-all-orderway-postdate-asc-DESC-page-1.html'),
-    ('阶段项目', '1.0', 'parser', 'xmfishParser', 'day20240812.phase_project.parser.XmfishParser'),
+
     ('阶段项目', '1.0', 'storage', 'DbStorage', 'day20240812.phase_project.storage.DbStorage'),   -- ConsoleStorage、FileStorage
     ('阶段项目', '1.0', 'keywords', 'keywords', '小米,华为,闲置'),
     ('阶段项目', '1.0', 'notificator', 'ConsoleNotificator', 'day20240812.phase_project.notificator.ConsoleNotificator'),  -- EmailNotificator
@@ -34,4 +33,14 @@ VALUES
     ('阶段项目', '1.0', 'to', 'to', 'to.xxx@qq.com');
 
 
+CREATE TABLE `url_parse` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(255) NOT NULL,
+    `url` VARCHAR(255) NOT NULL,
+    `parser` VARCHAR(255) NOT NULL
+);
+
+INSERT INTO `url_parse` (`name`, `url`, `parser`)
+VALUES
+    ('厦门小鱼', 'http://bbs.xmfish.com/thread-htm-fid-55-search-all-orderway-postdate-asc-DESC-page-1.html', 'day20240812.phase_project.parser.XmfishParser')
 
